81bd0ddc2649ec2fd9a346bab5084088ab90619c
Damian Escobedo <damian050697@gmail.com>
2026-05-21 22:20:21 -0600

    hacky square animation

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
-const memory = new WebAssembly.Memory({
-  initial: 10,
-});
+class RenderQueue {
+  constructor() {
+    this.items = [];
+    this.isRunningRender = false;
+  }
 
-async function main() {
-  let importObject = {
-    env: {
-      consoleLog: (pointer, size) => {
-        const memory = new Uint8Array(wasmInstance.exports.memory.buffer);
-        console.log('size', size);
-        const charCodes = memory.subarray(pointer, pointer + size);
-        const output = String.fromCharCode(...charCodes)
-        //let output = "";
-        //for (let i = 0; i < size; ++i) {
-        //  output += String.fromCharCode(memory[pointer + i]);
-        //}
-        console.log("Wasm consoleLog: ", output);
-      },
+  enqueue(element) {
+    this.items.push(element)
+    this.render();
+  }
+
+  denqueue() {
+    return this.items.shift();
+  }
+
+  render() {
+    if (this.isRunningRender) return;
+    this.isRunningRender = true;
+    const frameTimeMs = 33;
+
+    const execCall = () => {
+      const fn = this.denqueue();
+      requestAnimationFrame(fn);
+      //fn()
+
+      if (this.items.length > 0) {
+        setTimeout(execCall, frameTimeMs);
+      } else {
+        this.isRunningRender = false;
+      }
     }
-  };
-  const { instance: wasmInstance }  = await WebAssembly.instantiateStreaming(fetch('build/main.wasm'), importObject);
-  wasmInstance.exports.main();
+
+    setTimeout(execCall, frameTimeMs);
+  }
 }
 
-main();
+document.addEventListener("DOMContentLoaded", () =>  {
+  const canvas = document.getElementById("myCanvas");
+  const renderQueue = new RenderQueue();
+
+  function clearCanvas() {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function fillRect(color, startX, startY, width, height) {
+    clearCanvas();
+    const ctx = canvas.getContext("2d");
+    const hexColorStr = "#" + color.toString(16);
+    ctx.fillStyle = hexColorStr;
+    ctx.fillRect(startX, startY, width, height);
+  };
+
+  function arc(centerX, centerY, radius, startAngle, endAngle, counterclockwise) {
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle, counterclockwise);
+    ctx.stroke();
+  }
+
+  function readStringFromMemory(memory, pointer, size) {
+    const charCodes = memory.subarray(pointer, pointer + size);
+    return String.fromCharCode(...charCodes);
+  }
+
+  async function main() {
+    let importObject = {
+      env: {
+        consoleLog: (pointer, size) => {
+          const memory = new Uint8Array(wasmInstance.exports.memory.buffer);
+          const output = readStringFromMemory(memory, pointer, size)
+          console.log("Wasm consoleLog: ", output);
+        },
+        canvas_clear: () => renderQueue.enqueue(() => clearCanvas()),
+        canvas_fillRect: (color, startX, startY, width, height) => renderQueue.enqueue(() => fillRect(color, startX, startY, width, height)),
+        canvas_arc: (centerX, centerY, radius, startangle, endAngle, counterclockwise) => renderQueue.enqueue(() => arc(centerX, centerY, radius, startangle, endAngle, counterclockwise)),
+      }
+    };
+    const { instance: wasmInstance }  = await WebAssembly.instantiateStreaming(fetch('build/main.wasm'), importObject);
+    wasmInstance.exports.main();
+  }
+
+  main();
+});
--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #define assert(expression) if (!(expression)) {__builtin_trap();}
 
-extern void consoleLog(char *string, int size) __attribute__((import_module("env"), import_name("consoleLog")));
+#define bool32 int
+#define PI 3.14159265358979323846
+
+// NOTE: macro to be able to use js functions more easily without having to write all of this:
+//   extern void function_name (args) __attribute__((import_module("env"), import_name("function_name")));
+// every time
+//  JS_FUNC(void, consoleLog, (char *string, int size)); // converts to:
+//  extern void consoleLog(char *string, int size) __attribute__((import_module("env"), import_name("consoleLog")));
+#define JS_FUNC(return_type, func_name, func_signature) extern return_type func_name func_signature __attribute__((import_module("env"), import_name(#func_name)));
+
+JS_FUNC(void, consoleLog, (char *string, int size));
+// TODO: maybe wrap this functions so there easier to user form here for most cases
+// and make these ones as close as posible to the canvas functionallity to allow for flexibility
+JS_FUNC(void, canvas_fillRect, (int color, int startX, int startY, int width, int height));
+JS_FUNC(void, canvas_arc, (int centerX, int centerY, float radious, float startAngle, float endAngle, bool32 counterclockwise));
+JS_FUNC(void, canvas_clear, ());
+
+// I think the compiler optimized this out
+//void sleepMs(int ms) {
+//    double start = now();
+//    volatile double t;
+//    while ((t = now() - start) < ms) {
+//        // wait
+//    }
+//}
 
 int stringLength(char *s, int maxLength) {
   int result = 0;
@@ -24,5 +48,12 @@ string buildString(char *value) {
 int main() {
   string output = buildString("Hello world\n");
   consoleLog(output.value, output.length);
+
+  for (int i = 0; i < 1000; ++i) {
+    //canvas_clear();
+    canvas_fillRect(0x555555, i % 100, 0, 100, 100);
+  }
+  //
+  //canvas_arc(95, 50, 40, 0, 2 * PI, 0);
   return 0;
 }
